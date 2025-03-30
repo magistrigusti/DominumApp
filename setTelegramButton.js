909d899315818482f15641aad09c27a169b1567f
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-03-30 06:25:30 +0300

    0.0.3c

--- setTelegramButton.js
+++ setTelegramButton.js
@@ -1,8 +1,11 @@
+import axios from 'axios';
+
+// Вставь сюда токен своего бота от BotFather
 const TOKEN = "7522646269:AAEzq9qtrh7crGr6Z0pvKdEvcUKEZ11nkLA";
 
 // Функция для установки кнопки
 async function setMenuButton() {
-  const url = "https://api.telegram.org/bot${TOKEN}/setChatMenuButton";
+  const url = `https://api.telegram.org/bot${TOKEN}/setChatMenuButton`;
 
   const data = {
     menu_button: {
