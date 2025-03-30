df65a89e23d057d4addfa0de96ec023794296771
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-03-30 06:30:55 +0300

    0.0.4

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -1,10 +1,23 @@
 import { Header } from "./Header"
 import { SendTx } from './SendTx';
 import { Settings } from './Settings';
+import bgImage from './assets/main-bg.jpg';
 
 export const MainPage = () => {
   return (
-    <div>
+    <div
+      style={{
+      backgroundImage: `url(${bgImage})`,
+      backgroundSize: 'cover',       // картинка займет всю область
+      backgroundRepeat: 'no-repeat', // без повторения
+      backgroundPosition: 'center',  // по центру
+      width: '100%',
+      height: '100vh',               // высота на весь экран
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    }}
+    >
       <Header />
       <SendTx />
       <Settings />
