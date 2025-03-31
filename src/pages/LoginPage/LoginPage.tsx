b5960806bfaa862bb0229d55824d68dfc3dd3c46
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-03-31 11:11:46 +0300

    0.1.1

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,12 +1,16 @@
 import { TonConnectButton } from "@tonconnect/ui-react";
+import ImgContract from "../../assets/contract_actral_island.png";
 
 
 export const LoginPage = () => {
   return (
     <div>
+      <TonConnectButton />
+
       <h2>to go through connecting a wallet.</h2>
 
-      <TonConnectButton />
+      <img src={ImgContract} alt="" />
+      
     </div>
   )
 }
--- src/pages/MenuPage/MenuPage.tsx
+++ src/pages/MenuPage/MenuPage.tsx
@@ -1,8 +1,10 @@
 import { HomePage } from "../HomePage/HomePage";
+import { TonConnectButton } from "@tonconnect/ui-react";
 
 export const MenuPage = () => {
   return (
     <div>
+      <TonConnectButton />
       <h2>Welcome to the crypto space Dominum which is the entrance to the crypto meta universe Allodium</h2>
       
       <HomePage />
