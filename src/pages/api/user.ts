6b9915ff5293f01812b2ff4e62d9597bb63d00e4
magistrigusti <47912728+magistrigusti@users.noreply.github.com>
2025-04-07 07:15:16 +0300

    conect

--- src/pages/api/user.ts
+++ src/pages/api/user.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 import dbConnect from '@/lib/dbConnect';
 import { UserModel } from '@/models/UserModel';
 
+
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method Not Allowed' });
